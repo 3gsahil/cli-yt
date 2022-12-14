cf7220ec5a180ea8141a5c7737f35eae06d0ed21
Sahil Gedam <119723149+xahilg18@users.noreply.github.com>
2022-12-14 15:06:03 +0530

    initial config

---
+++ cli-yt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+figlet wlcm to yt.cli
+
+
+date
+
+start="enter an title to "
+
+figlet group no. 4
+
+echo $start
+
+
+
+#"ytfzf"
+
+#query=$(printf '%s' "$*" | tr ' ' '+' )
+
+ytfzf
