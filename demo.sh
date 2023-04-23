90a75e30c14c1397e329b8487e0ade75f5e667bd
Sahil Gedam <119723149+xahilg18@users.noreply.github.com>
2023-04-23 16:13:33 +0530

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/bash
+
+wcm="wellcome to demo of command line youtube"
+
+#the wellcome message to greet the user of the script or program
+
+date
+
+#enlist the date for reminding the project incharge on the day of presentation 
+
+cowsay $wcm
+#an unique generation of greeting by using the gnu and linux binary utility cowsay
+#the 
+
+#the video demonstration of the program 
+cd ~/Documents
+
+tree .
+
+#representing the tree structure inside the folder for the retrospective view of the directory 
+
+clear
+
+#clear the terminal screer for better visual appeal 
+
+sleep 2
+
+#the program will wait for 2 seconds of input 
+
+cd Mini\ Project/
+
+#directory sequential jump 1
+
+
+sleep 1
+
+#an one second pause for the program
+
+sleep 1
+
+#another stand-by pause of 1000 ms 
+
+cd mini\ projekt/
+
+#changing into the sequentional directory 
+
+
+sleep 2
+
+#an 2 second pause 
+
+
+#the player rendering will happen here at the time of demo 
+
+mpv WhatsApp\ Video\ 2022-11-16\ at\ 9.06.43\ AM.mp4
+
+#rendering the video through the mpv linux media player 
+
+
+#revert back to original directory for allowing the further demo to be run and allowing the execution of the program
+
+cd 
+
+#changing to the original master directory 
+
+sleep 2
+
+#2 second pause 
+
+cd
+
+#an assurance that we are in the right working directory
+
+pwd
+
+#checking which is the present working directory right now 
+
+sleep 1
+
+#an one second pause 
+
+
+
+#the live demo
+cd ytcli
+
+#jumping to the program directory
+
+
+ls
+
+#complementary listing 
+
+
+sleep 1
+
+#one second of pause
+
+
+ls -a
+
+#listing the files again
+
+sleep 2
+
+#an 2 second pause 
+
+./cli-yt.sh
+
+
